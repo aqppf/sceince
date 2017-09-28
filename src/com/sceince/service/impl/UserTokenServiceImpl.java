bfd62f37b5c72d50f89fe92cb666f3b46b04b13f
aqppf <aqppf@163.com>
2017-09-28 17:23:20 +0800

    modify

--- src/com/sceince/service/impl/UserTokenServiceImpl.java
+++ src/com/sceince/service/impl/UserTokenServiceImpl.java
@@ -2,7 +2,6 @@ package com.sceince.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sceince.comm.util.MsgUtil;
